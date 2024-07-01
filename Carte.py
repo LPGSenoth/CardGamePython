eb3d7e4fbfb042f1c3dcf13015c00e850847867d
WanDayoMollets <65540277+WanDayoMollets@users.noreply.github.com>
2024-07-01 10:24:33 +0200

    cartes

--- Carte.py
+++ Carte.py
@@ -2,6 +2,8 @@ class Carte:
     def __init__(self, valeur, couleur):
         self.couleur = couleur
         self.valeur = valeur
+        self.card_values = {"7": 7, "8": 8, "9": 9,"10": 10, "V": 11, "R": 12, "AS": 13}
+        self.card_colors = {"Coeur": 4, "Pique": 3, "Carreau": 2, "Trefle": 1}
     
     def __str__(self):
         return f"{self.valeur}_{self.couleur}"
@@ -10,4 +12,11 @@ class Carte:
         return self.valeur == o_card.valeur and self.couleur == o_card.couleur
 
     def __gt__(self, o_card: object) -> bool:
-        return self.valeur > o_card.valeur
+        return (False if (self == o_card) else (True if (self.valeur == o_card.valeur and self.card_colors[self.couleur] > o_card.card_colors[o_card.couleur]) else (True if self.card_values[self.valeur] > self.card_values[o_card.valeur] else False)))
+    
+t = Carte("AS", "Trefle")
+print(t)
+t2 = Carte("R", "Trefle")
+print(t2)
+print(t == t2)
+print(t > t2)
