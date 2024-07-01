aa5afbe37f17c2313fb4ceb60d499f99178459f1
LPGSenoth <163125498+LPGSenoth@users.noreply.github.com>
2024-07-01 11:43:53 +0200

    main config

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("caca")
+import Jeu, Joueur
+
+toto = Joueur.Joueur('Toto')
+titi = Joueur.Joueur('Titi')
+donald = Joueur.Arbitre('Donald')
+donald.donne_cartes_melangees_joueur(toto)
+donald.donne_cartes_melangees_joueur(titi)
+partie = Jeu.Jeu(joueur1=toto, joueur2=titi, arbitre=donald)
+partie.imprimer_jeu_de_cartes()
+partie.simuler_jeu()
+partie.mettre_a_jour_vainqueur()
+partie.donner_resultats()
