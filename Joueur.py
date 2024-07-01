333ad1e1e42c755af2003cac454df23b99873d22
S4medi <samdelestre@gmail.com>
2024-07-01 09:38:58 +0200

    jj

---
+++ Joueur.py
@@ -0,0 +1,7 @@
+class Joueur :
+    def __init__(self,nom,points,nombre_parties_gagnees):
+        self.nom = nom
+        self.points = points
+        self.nombre_parties_gagnees = nombre_parties_gagnees
+
+        
