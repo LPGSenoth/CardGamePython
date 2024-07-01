13bff3276ae65e4b549fa574812b6c90dae0a2a3
S4medi <samdelestre@gmail.com>
2024-07-01 10:10:19 +0200

    j

--- Joueur.py
+++ Joueur.py
@@ -1,7 +1,35 @@
-class Joueur :
-    def __init__(self,nom,points,nombre_parties_gagnees):
+
+
+class Personne :
+    def __init__(self,nom,points= 0, nombre_parties_gagnees = 0):
         self.nom = nom
         self.points = points
         self.nombre_parties_gagnees = nombre_parties_gagnees
 
-    
+
+class Joueur(Personne):
+    def __init__(self,nom,points = 0,nombre_parties_gagnees = 0,cartes = None):
+        Personne.__init__(self, nom,points,nombre_parties_gagnees)
+        self.cartes = cartes
+
+    def __str__(self):
+        return f"le joueur sappelle {self.nom}"
+
+
+class Arbitre(Personne):
+    def __init__(self,nom,points=0,nombre_parties_gagnees=0):
+        Personne.__init__(self, nom, points, nombre_parties_gagnees)
+
+    def __str__(self):
+        return f"l'arbitre s'appelle {self.nom}"
+
+
+    def donne_cartes_melangees_joueur(self,Joueur):
+        
+
+
+toto = Joueur('toto')
+dodo = Arbitre('dodo')
+
+print(toto)
+print(dodo)
